03b904c16c241f422c130d2df6b922b6c68f3af3
Christopher Sherman <sherman27@llnl.gov>
2021-04-30 15:23:26 -0700

    Adding microseismic analysis options to pygeosx_tools

--- src/coreComponents/python/modules/pygeosx_tools_package/setup.py
+++ src/coreComponents/python/modules/pygeosx_tools_package/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='pygeosx_tools',
-      version='0.0.1',
+      version='0.1.0',
       description='Tools for interacting with pygeosx',
       author='Chris Sherman',
       author_email='sherman27@llnl.gov',
       packages=['pygeosx_tools'],
-      install_requires=['matplotlib'])
+      install_requires=['matplotlib', 'pyevtk'])
 
